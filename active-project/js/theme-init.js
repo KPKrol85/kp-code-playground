b4715680533217055ea2e7e67ac823691c9dacc1
KPKrol85 <kpkrol85@gmail.com>
2026-01-19 18:51:01 +0100

    deleted all files

--- active-project/js/theme-init.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  const stored = window.safeStorage?.safeGetJSON("kp_theme", null);
-  const theme =
-    stored ??
-    (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-  document.documentElement.setAttribute("data-theme", theme);
-})();
--- active-project/js/utils/sanitize.js
+++
@@ -1,6 +0,0 @@
-export const sanitizeText = (value) => {
-  if (!value) {
-    return "";
-  }
-  return value.replace(/[<>]/g, "");
-};
