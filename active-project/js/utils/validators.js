a0f9880e78cdf8ec0709178857abd3e18374aee9
KPKrol85 <kpkrol85@gmail.com>
2026-01-24 15:46:54 +0100

    chore: remove active-project folder
    * remove all files from active-project directory
    * clean repository for next iteration and audit work

--- active-project/js/utils/validators.js
+++
@@ -1,5 +0,0 @@
-export const validators = {
-  required: (value) => value && value.trim().length > 0,
-  email: (value) => /\S+@\S+\.\S+/.test(value),
-  minLength: (length) => (value) => value && value.length >= length,
-};
