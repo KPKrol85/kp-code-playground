949de529845d6ec15ccd06cf62ecd807085f2cc2
KPKrol85 <104792533+KPKrol85@users.noreply.github.com>
2026-02-09 18:54:39 +0100

    Add NovaLearn Academy multi-page site

---
+++ education/html-css-js/pr-02-novalearn-academy/js/main.js
@@ -0,0 +1,9 @@
+import { initNav } from "./modules/nav.js";
+import { initAccordion } from "./modules/accordion.js";
+import { initCourseFilters } from "./modules/course-filters.js";
+import { initFormValidation } from "./modules/form-validate.js";
+
+initNav();
+initAccordion();
+initCourseFilters();
+initFormValidation();
