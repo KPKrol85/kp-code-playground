d70abc2c8b9b8fd4185ad095e9be689c954733b1
KPKrol85 <kpkrol85@gmail.com>
2026-02-10 19:05:10 +0100

    Remove completed project folders from playground repo
    * Removed finalized projects already migrated to the target repository
    * Cleaned playground root to keep only active and experimental work

--- it-pr01-vertexdigital/js/utils.js
+++
@@ -1,4 +0,0 @@
-export const prefersReducedMotion = () =>
-  window.matchMedia('(prefers-reduced-motion: reduce)').matches;
-
-export const isElement = (value) => value instanceof Element;
--- pr-01-vireon-clinic/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initNav } from "./modules/nav.js";
-import { initAccordion } from "./modules/accordion.js";
-import { initFormValidation } from "./modules/form-validate.js";
-import { initTreatmentFilter } from "./modules/filters.js";
-
-initNav();
-initAccordion();
-initFormValidation();
-initTreatmentFilter();
