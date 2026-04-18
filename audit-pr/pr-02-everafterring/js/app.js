ea1127838a09676b1269b28edddbfe8b2744e9df
KPKrol85 <kpkrol85@gmail.com>
2026-04-18 18:31:56 +0200

    relocate project in to local data

--- audit-pr/pr-02-everafterring/js/app.js
+++
@@ -1,8 +0,0 @@
-import { onReady } from './modules/dom.js';
-import { initNav } from './modules/nav.js';
-import { initForm } from './modules/form.js';
-
-onReady(() => {
-  initNav();
-  initForm();
-});
